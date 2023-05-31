5777f169f84c76945f46f63a9b12d2ec7264b06e
201007939 <lee0lerma@gmail.com>
2023-05-31 13:57:06 -0700

    soundboard

--- app/src/main/java/com/example/soundboard/MainActivity.java
+++ app/src/main/java/com/example/soundboard/MainActivity.java
@@ -2,13 +2,42 @@ package com.example.soundboard;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Button btnBurke;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnBurke = findViewById(R.id.btn_burke);
+
+
+        btnBurke.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                playBurke();
+            }
+        });
+    }
+
+
+
+    private void playBurke(){
+        MediaPlayer mp = MediaPlayer.create(this,R.raw.sample_leo_burke);
+        mp.start();
+        mp.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mp) {
+                mp.stop();
+                mp.release();
+                mp = null;
+            }
+        });
     }
 }
