b49d3d569e04580ee4bc36bba1fff26e45e11d8f
201007939 <lee0lerma@gmail.com>
2023-06-02 14:01:19 -0700

    weenie

--- app/src/main/java/com/example/soundboard/MainActivity.java
+++ app/src/main/java/com/example/soundboard/MainActivity.java
@@ -9,14 +9,25 @@ import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Button btnBurke;
+    private Button btnBurke, btnFabrizio, btnDarren, btnDoorbell, btnSpring, btnPunch, btnPunch2, btnShatter, btnDuck, btnCricket;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         btnBurke = findViewById(R.id.btn_burke);
+        btnFabrizio = findViewById(R.id.btn_fabrizio);
+        btnDarren = findViewById(R.id.btn_darren);
+        btnDoorbell = findViewById(R.id.btn_doorbell);
+        btnSpring = findViewById(R.id.btn_spring);
 
+
+        btnFabrizio.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                playFabrizio();
+            }
+        });
 
         btnBurke.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -24,6 +35,28 @@ public class MainActivity extends AppCompatActivity {
                 playBurke();
             }
         });
+        btnDarren.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                playDarren();
+            }
+        });
+
+        btnDoorbell.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                playDoorbell();
+            }
+        });
+
+        btnSpring.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                playSpring();
+            }
+        });
+
+
     }
 
 
