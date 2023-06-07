bf3c190d59b5f8de366d2e6be5027157cdd234a3
201007939 <lee0lerma@gmail.com>
2023-06-07 10:21:00 -0700

    weenie

--- app/src/main/java/com/example/soundboard/SoundBoardLeo.java
+++ app/src/main/java/com/example/soundboard/SoundBoardLeo.java
@@ -20,6 +20,19 @@ public class SoundBoardLeo extends AppCompatActivity {
         btnDarren = findViewById(R.id.btn_darren);
         btnDoorbell = findViewById(R.id.btn_doorbell);
         btnDuck = findViewById(R.id.btn_duck);
+        btnGong = findViewById(R.id.btn_gong);
+        btnPunch = findViewById(R.id.btn_punch);
+        btnPunch2 = findViewById(R.id.btn_punch2);
+        btnShatter = findViewById(R.id.btn_shatter);
+        btnSpring = findViewById(R.id.btn_spring);
+        btnToy = findViewById(R.id.btn_toy);
+
+        btnBurke.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                playBurke();
+            }
+        });
 
 
 
@@ -30,6 +43,136 @@ public class SoundBoardLeo extends AppCompatActivity {
 
     private void playBurke(){
         MediaPlayer mp = MediaPlayer.create(this,R.raw.sample_leo_burke);
+        mp.start();
+        mp.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mp) {
+                mp.stop();
+                mp.release();
+                mp = null;
+            }
+        });
+    }
+
+    private void playCrickets(){
+        MediaPlayer mp = MediaPlayer.create(this,R.raw.sample_leo_crickets);
+        mp.start();
+        mp.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mp) {
+                mp.stop();
+                mp.release();
+                mp = null;
+            }
+        });
+    }
+
+    private void playDarren(){
+        MediaPlayer mp = MediaPlayer.create(this,R.raw.sample_leo_darren);
+        mp.start();
+        mp.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mp) {
+                mp.stop();
+                mp.release();
+                mp = null;
+            }
+        });
+    }
+
+    private void playDoorbell(){
+        MediaPlayer mp = MediaPlayer.create(this,R.raw.sample_leo_doorbell);
+        mp.start();
+        mp.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mp) {
+                mp.stop();
+                mp.release();
+                mp = null;
+            }
+        });
+    }
+
+    private void playDuck(){
+        MediaPlayer mp = MediaPlayer.create(this,R.raw.sample_leo_duck);
+        mp.start();
+        mp.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mp) {
+                mp.stop();
+                mp.release();
+                mp = null;
+            }
+        });
+    }
+
+    private void playGong(){
+        MediaPlayer mp = MediaPlayer.create(this,R.raw.sample_leo_gong);
+        mp.start();
+        mp.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mp) {
+                mp.stop();
+                mp.release();
+                mp = null;
+            }
+        });
+    }
+
+    private void playPunch(){
+        MediaPlayer mp = MediaPlayer.create(this,R.raw.sample_leo_punch);
+        mp.start();
+        mp.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mp) {
+                mp.stop();
+                mp.release();
+                mp = null;
+            }
+        });
+    }
+
+    private void playPunch2(){
+        MediaPlayer mp = MediaPlayer.create(this,R.raw.sample_leo_punch2);
+        mp.start();
+        mp.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mp) {
+                mp.stop();
+                mp.release();
+                mp = null;
+            }
+        });
+    }
+
+    private void playShatter(){
+        MediaPlayer mp = MediaPlayer.create(this,R.raw.sample_leo_shatter);
+        mp.start();
+        mp.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mp) {
+                mp.stop();
+                mp.release();
+                mp = null;
+            }
+        });
+    }
+
+    private void playSpring(){
+        MediaPlayer mp = MediaPlayer.create(this,R.raw.sample_leo_spring);
+        mp.start();
+        mp.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+            @Override
+            public void onCompletion(MediaPlayer mp) {
+                mp.stop();
+                mp.release();
+                mp = null;
+            }
+        });
+    }
+
+    private void playToy(){
+        MediaPlayer mp = MediaPlayer.create(this,R.raw.sample_leo_toy);
         mp.start();
         mp.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
             @Override
